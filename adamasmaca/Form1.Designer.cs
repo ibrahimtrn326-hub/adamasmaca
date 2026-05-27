eaad1a01d206973e598a77acc4e2c04ca5cfe9b9
ibrahimtrn326-hub <ibrahimtrn326@gmail.com>
2026-05-27 19:24:20 +0300

    Proje dosyası ekle.

---
+++ adamasmaca/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace adamasmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(96, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(_ _ _ _ _ _)";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "harf tahmin et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1270, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "kalan canlar";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(942, 135);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(390, 404);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(411, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 52);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Yeni Oyun,";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(268, 191);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 52);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "kelime tahmin";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1288, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(548, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 46);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 619);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ adamasmaca/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adamasmaca
+{
+    public partial class Form1 : Form
+    {
+        string[] kelimeler = { "BILGISAYAR", "YAZILIM", "KLAVYE", "MONITOR", "INTERNET" };
+
+        Random rnd = new Random();
+
+        string secilenKelime;
+        char[] gorunenKelime;
+
+        int can = 6;
+        int skor = 0;
+        int yanlis = 0;
+        string[] asama =
+{
+    "🙂",
+    "😐",
+    "😕",
+    "😟",
+    "😨",
+    "😰",
+    "💀"
+};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            skor = 0;
+            label3.Text = "Skor: 0";
+
+            YeniOyun();
+            YeniOyun();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            char harf = Convert.ToChar(textBox1.Text.ToUpper());
+
+            bool bulundu = false;
+
+            for (int i = 0; i < secilenKelime.Length; i++)
+            {
+                if (secilenKelime[i] == harf)
+                {
+                    gorunenKelime[i] = harf;
+                    bulundu = true;
+                }
+            }
+
+            if (!bulundu)
+            {
+
+                can--;
+                yanlis++;
+
+                label4.Text = asama[yanlis];
+            }
+
+            label1.Text = string.Join(" ", gorunenKelime);
+
+            listBox1.Items.Add(harf);
+
+            textBox1.Clear();
+
+            if (!label1.Text.Contains("_"))
+            {
+                MessageBox.Show("Kazandın!");
+            }
+
+            if (can == 0)
+            {
+                MessageBox.Show("Kaybettin! Kelime: " + secilenKelime);
+            }
+        }
+        void YeniOyun()
+        {
+            secilenKelime = kelimeler[rnd.Next(kelimeler.Length)];
+
+            gorunenKelime = new char[secilenKelime.Length];
+
+            for (int i = 0; i < gorunenKelime.Length; i++)
+            {
+                gorunenKelime[i] = '_';
+            }
+
+            can = 6;
+            yanlis = 0; // 👈 BURAYA
+            label4.Text = asama[yanlis]; // 👈 BURAYA
+
+            label2.Text = "Can: " + can;
+            label1.Text = string.Join(" ", gorunenKelime);
+
+            listBox1.Items.Clear();
+            textBox1.Clear();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            YeniOyun();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string tahmin = textBox1.Text.ToUpper();
+
+            if (tahmin == secilenKelime)
+            {
+                label1.Text = string.Join(" ", secilenKelime.ToCharArray());
+                MessageBox.Show("🎉 Kazandın!");
+                skor += 10;
+                label3.Text = "Skor: " + skor;
+            }
+            else
+            {
+                can--;
+                label2.Text = "Can: " + can;
+                MessageBox.Show("❌ Yanlış kelime!");
+            }
+
+            if (can == 0)
+            {
+                MessageBox.Show("Kaybettin! Kelime: " + secilenKelime);
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
